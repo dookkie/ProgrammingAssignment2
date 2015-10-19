e4ad6a8885de7cb3cc11ca93ab1511bffbb00c23
dookkie <tmswlj@gmail.com>
2015-10-19 21:01:48 +0800

    Assignment 2
    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## User inputs a matrix (E.g. makeCacheMatrix(matrix(2:5,2,2)) ).
+## Program check if prior calculation has been done on this set of matrix.
+## If calculation has been done before, program will print the cached matrix.
+## If not, progrm will calculate, store the value and print the value.
 
-## Write a short comment describing this function
 
+## Input the matrix, pass it to cacheSolve for calculation.
+## Calculated value is stored.
+Oldx<-matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        Inv<-cacheSolve(x)
+        
+         Inv <<- Inv
+print(Inv)
+        
 }
 
 
-## Write a short comment describing this function
+## Check if prior calculation has been carried out on this set of matrix.
+## Will retrive cached calculation if so,
+## If not, calculation be carried out
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i<-identical(Oldx,x)
+        if(i){
+                message("getting cached data")
+                return(Inv)
+        }
+        Inv<-solve(x)
+        Oldx<<-x
+        Inv
 }
+
